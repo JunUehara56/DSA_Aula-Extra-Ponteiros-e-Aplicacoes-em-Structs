cd89d2c4630c37abab8765fc03eecf5a3af54725
JunUehara56 <jun.uehara.br@gmail.com>
2026-08-24 11:53:19 -0300

    Adiciona cadastro e exibicao do livro

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char nome[50];
+    char email[50];
+}Usuario;
+
+typedef struct {
+    char titulo[50];
+    char autor[50];
+    int codigo;
+    char status[20];
+    Usuario usuario;
+}Livro;
+
+int main() {
+    Livro livro1;
+    Usuario usuario1;
+    strcpy(usuario1.nome, "Joao Silva");
+    strcpy(usuario1.email, "joao.silva@gmail.com");
+    strcpy(livro1.titulo, "O Hobbit");
+    strcpy(livro1.autor, "J.R.R. Tolkien");
+    livro1.codigo = 101;
+    strcpy(livro1.status, "disponivel");
+    printf("===== LIVRO =====\n");
+    printf("Titulo: %s\n", livro1.titulo);
+    printf("Autor: %s\n", livro1.autor);
+    printf("Codigo: %d\n", livro1.codigo);
+    printf("Status: %s\n", livro1.status);
+    return 0;
+}
+
